4aea93427bb394ddc2814570279621ec350fafe0
Pedro Alfonso <p.alfonsoag@gmail.com>
2020-07-19 22:33:03 -0400

    Weather component enfoque TDD

---
+++ src/components/Weather/Weather.stories.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Weather from "./Weather";
+
+export const WeatherExample = () => <Weather temperature={10} />;
+
+export default {
+  title: "Weather",
+  component: Weather,
+};
