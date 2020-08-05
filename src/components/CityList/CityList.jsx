def79034918fb7aec57331818d44e3897750139d
Pedro Alfonso <p.alfonsoag@gmail.com>
2020-08-05 00:24:10 -0400

    convert units

--- src/components/CityList/CityList.jsx
+++ src/components/CityList/CityList.jsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import convertUnits from "convert-units";
 import Grid from "@material-ui/core/Grid";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
+import Alert from "@material-ui/lab/Alert";
 import PropTypes from "prop-types";
 import CityInfo from "../CityInfo";
 import Weather from "../Weather";
@@ -19,11 +21,10 @@ const renderCityAndCountry = (eventOnClickCity) => (
           <CityInfo city={city} country={country} />
         </Grid>
         <Grid item md={3} xs={12}>
-          {weather ? (
-            <Weather temperature={weather.temperature} state={weather.state} />
-          ) : (
-            "No data exist"
-          )}
+          <Weather
+            temperature={weather && weather.temperature}
+            state={weather && weather.state}
+          />
         </Grid>
       </Grid>
     </ListItem>
@@ -32,32 +33,56 @@ const renderCityAndCountry = (eventOnClickCity) => (
 
 const CityList = ({ cities, onClickCity }) => {
   const [allWeather, setAllWeather] = useState({});
-  const apiKey = `8250a74fdc654139b831451a9adcf0a2`;
+  const [error, setError] = useState(null);
+  const apiKey = process.env.REACT_APP_API_KEY_WEATHER;
   useEffect(() => {
     cities.forEach(({ city, country, countryCode }) => {
       const url = `https://api.openweathermap.org/data/2.5/weather?q=${city},${countryCode}&appid=${apiKey}`;
 
-      axios.get(url).then((response) => {
-        const { data } = response;
-        const temperature = data.main.temp;
-        const state = data.weather[0].main.toLowerCase();
-        setAllWeather((allWeather) => ({
-          ...allWeather,
-          [`${city}-${country}`]: { temperature, state },
-        }));
-      });
+      axios
+        .get(url)
+        .then((response) => {
+          const { data } = response;
+          setError(null);
+          const temperature = Number(
+            convertUnits(data.main.temp).from("K").to("C").toFixed(0)
+          );
+          const state = data.weather[0].main.toLowerCase();
+          setAllWeather((allWeather) => ({
+            ...allWeather,
+            [`${city}-${country}`]: { temperature, state },
+          }));
+        })
+        .catch((error) => {
+          if (error.response) {
+            setError("There was an error in the weather service");
+          } else if (error.request) {
+            setError(
+              "There was an error reaching the service, check your internet connection"
+            );
+          } else {
+            setError("There was an unexpected error");
+          }
+        });
     });
   }, [cities, apiKey]);
   const renderCityInfo = renderCityAndCountry(onClickCity);
   return (
-    <List>
-      {cities.map((cityAndCountry) =>
-        renderCityInfo(
-          cityAndCountry,
-          allWeather[`${cityAndCountry.city}-${cityAndCountry.country}`]
-        )
+    <>
+      {error && (
+        <Alert severity="error" onClose={() => setError(null)}>
+          {error}
+        </Alert>
       )}
-    </List>
+      <List>
+        {cities.map((cityAndCountry) =>
+          renderCityInfo(
+            cityAndCountry,
+            allWeather[`${cityAndCountry.city}-${cityAndCountry.country}`]
+          )
+        )}
+      </List>
+    </>
   );
 };
 
--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
+import Skeleton from "@material-ui/lab/Skeleton";
 import { IconContext } from "react-icons";
 import IconState, { validValues } from "../IconState";
 
@@ -16,18 +17,26 @@ const Weather = ({ temperature, state }) => {
       spacing={1}
     >
       <IconContext.Provider value={{ size: "5em" }}>
-        <IconState state={state} />
+        {state ? (
+          <IconState state={state} />
+        ) : (
+          <Skeleton variant="circle" height={80} width={80} />
+        )}
       </IconContext.Provider>
-      <Typography display="inline" variant="h3">
-        {temperature}
-      </Typography>
+      {temperature ? (
+        <Typography display="inline" variant="h3">
+          {temperature} °C
+        </Typography>
+      ) : (
+        <Skeleton variant="rect" height={80} width={80} />
+      )}
     </Grid>
   );
 };
 
 Weather.propTypes = {
-  temperature: PropTypes.number.isRequired,
-  state: PropTypes.oneOf(validValues).isRequired,
+  temperature: PropTypes.number,
+  state: PropTypes.oneOf(validValues),
 };
 
 export default Weather;
